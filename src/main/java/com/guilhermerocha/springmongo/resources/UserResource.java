f6a6e5e5237adb7dfcac2eefe43e5a33fc436037
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-01-25 23:37:05 -0300

    Entity user e REST funcionando

---
+++ src/main/java/com/guilhermerocha/springmongo/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.guilhermerocha.springmongo.resources;
+
+import com.guilhermerocha.springmongo.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll(){
+        User maria = new User("1", "Maria bonete", "maria@io");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(maria));
+        return ResponseEntity.ok().body(list);
+    }
+}
