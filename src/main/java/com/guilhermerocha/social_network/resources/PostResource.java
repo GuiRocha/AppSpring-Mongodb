ea89eef87defe3cab980c72f97f403010aae7e57
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-05-30 14:56:54 -0300

    Adicionando Swagger UI e primerio teste em 'post'

--- src/main/java/com/guilhermerocha/social_network/resources/PostResource.java
+++ src/main/java/com/guilhermerocha/social_network/resources/PostResource.java
@@ -2,17 +2,22 @@ package com.guilhermerocha.social_network.resources;
 
 import com.guilhermerocha.social_network.domain.Post;
 import com.guilhermerocha.social_network.services.PostService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(value = "posts")
 @RequestMapping(value = "/posts")
 public class PostResource {
     @Autowired
     private PostService postService;
 
-    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    @ApiOperation(value = "Controller posts")
+    @RequestMapping(value="/{id}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
