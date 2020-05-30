e8527d0988984bfc7c6d9ffa7e1f34980fc63356
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-05-30 13:11:34 -0300

    Refatorando config e gitgnore

--- src/main/java/com/guilhermerocha/social_network/config/Instantiation.java
+++ src/main/java/com/guilhermerocha/social_network/config/Instantiation.java
@@ -29,16 +29,25 @@ public class Instantiation implements CommandLineRunner {
         simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 
-        User maria = new User(null, "Maria billy", "Guilhermerxcha@gmail.com");
-        userRepository.saveAll(Arrays.asList(maria));
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post guilherme = new Post(null, simpleDateFormat.parse("21/02/2019"), "Partiu viagem", "Olá as festas foram muito boas", new AuthorDTO(maria));
-        postRepository.saveAll(Arrays.asList(guilherme));
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        CommentDTO comentario01 = new CommentDTO("Boa viagem", simpleDateFormat.parse("21/02/2019"), new AuthorDTO(maria));
-        guilherme.getCommentList().addAll(Arrays.asList(comentario01));
+        Post post1 = new Post(null, simpleDateFormat.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, simpleDateFormat.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-        maria.getPostList().addAll(Arrays.asList(guilherme));
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", simpleDateFormat.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", simpleDateFormat.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", simpleDateFormat.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getCommentList().addAll(Arrays.asList(c1, c2));
+        post2.getCommentList().addAll(Arrays.asList(c3));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPostList().addAll(Arrays.asList(post1, post2));
         userRepository.save(maria);
 
     }
