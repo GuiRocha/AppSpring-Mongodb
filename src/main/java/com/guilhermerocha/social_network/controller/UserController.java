4c953dcb13063158a2f88c19b644bd9a798d9543
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-04 16:29:25 -0300

    Inserindo método PUT na aplicacao

--- src/main/java/com/guilhermerocha/social_network/controller/UserController.java
+++ src/main/java/com/guilhermerocha/social_network/controller/UserController.java
@@ -53,7 +53,15 @@ public class UserController {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
-    //TODO: Adicionar método PUT
+
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
     @ApiOperation(value = "Busca posts relacionados ao usuario atraves do id")
     @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
     public ResponseEntity<List<Post>> findByPosts(@PathVariable String id) {
